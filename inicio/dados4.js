6638c554d7e2e3a39e40657b15f0650c5abbf92a
Victor Puntel <victor.a.puntel@gmail.com>
2024-08-06 01:35:42 -0300

    Dados 4 + 2 desafios

---
+++ inicio/dados4.js
@@ -0,0 +1,13 @@
+let a = 3
+
+a = a + 10 // aqui é um exemplo de que modificamos o valor da variavel por algum motivo
+
+console.log(a);
+
+
+//--------//
+const b = 3
+
+// b = b + 10 não podemos alterar um valor constante de forma brusca como aqui!
+
+console.log(b);
---
+++ inicio/desafio_area.js
@@ -0,0 +1,7 @@
+// fórmula = PI * raio * raio
+const PI = 3.14
+let  raio = 10
+
+let area = PI * raio * raio
+
+console.log('A área é ' + area + 'm2');
