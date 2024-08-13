cfc6209a152a9cdc7ab328623002fad25d424f6f
Victor Puntel <victor.a.puntel@gmail.com>
2024-08-13 00:25:40 -0300

    Estrutura de controle IF/ELSE e ELSE IF

---
+++ exercicios-js/controle/if1.js
@@ -0,0 +1,27 @@
+function soBoaNoticia(nota) {
+    if(nota >= 7) {
+        console.log('Aprovado com ' + nota);
+    } else console.log('reprovado com ' + nota);
+}
+
+soBoaNoticia(7)
+soBoaNoticia(6.9)
+
+function seForVerdadeEuFalo(valor) {
+    if(valor) {
+        console.log('É verdadeiro... ' + valor);
+    }
+}
+
+seForVerdadeEuFalo()
+seForVerdadeEuFalo(null)
+seForVerdadeEuFalo(undefined)
+seForVerdadeEuFalo(NaN)
+seForVerdadeEuFalo('')
+seForVerdadeEuFalo(0)
+seForVerdadeEuFalo(8)
+seForVerdadeEuFalo(' ')
+seForVerdadeEuFalo('?')
+seForVerdadeEuFalo([])
+seForVerdadeEuFalo([1,2])
+seForVerdadeEuFalo({})
