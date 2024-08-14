7c02e4b8e2c5774bc02bb3ac85bf873c46113eca
Victor Puntel <victor.a.puntel@gmail.com>
2024-08-14 02:46:39 -0300

    Estrutura de contorle Switch, While e Do While

---
+++ exercicios-js/controle/while.js
@@ -0,0 +1,12 @@
+function getInteiroAleatorioEntre(min, max) {
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+let opcao = 0
+
+while(opcao != -2) {
+    opcao = getInteiroAleatorioEntre(-1, 10)
+    console.log('Valor sorteado:' + opcao);
+}
+console.log('Obrigado por participar!');
