65a80d9121bbafedcd46dda56333c80b4c37ed11
Victor Puntel <victor.a.puntel@gmail.com>
2024-08-17 21:48:37 -0300

    Funções arrow

---
+++ exercicios-js/funcao/arrowFunction1.js
@@ -0,0 +1,18 @@
+let dobro = function(a) {
+    return 2 * a
+}
+
+dobro = a => {
+    return 2 * a
+}
+
+dobro = a => 2 * a //retur implicito
+console.log(dobro(Math.PI));
+
+let ola = function() {
+    return 'Olá'
+}
+
+ola = () => 'Olá'
+ols = _ => 'Olá' //possui um parametro
+console.log(ola());
