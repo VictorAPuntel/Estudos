8b2742e9ce9a981c42034ab8b093c82fa99c7e95
Victor Puntel <victor.a.puntel@gmail.com>
2024-09-02 23:44:01 -0300

    Exemplo de concat

---
+++ exercicios-js/array/concat.js
@@ -0,0 +1,6 @@
+const filhas = ["Maria", "Elena"];
+const filhos = ["Eduardo", "Rafael"];
+const todos = filhas.concat(filhos, "Peter");
+console.log(todos, filhas, filhos);
+
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]));
